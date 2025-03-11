a21f9d76f8d5d66ff0cf6040d1e0a024c738d8ef
Sena <malzatej257@gmail.com>
2025-03-11 17:21:19 -0500

    deploy 6

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ export function App() {
       <div className="main-container">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/Home" element={<Home />} />
           <Route path="/TechnicalSkills" element={<TechnicalSkills />} />
           <Route path="/SocialSkills" element={<SocialSkills />} />
           <Route path="/ProfessionalExperience" element={<ProfessionalExperience />} />
