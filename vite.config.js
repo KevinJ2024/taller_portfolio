83bf47c16947694021dff385090726b23858033d
Kevin Julian Alvarez Ramirez <lukitor1414@gmail.com>
2025-03-11 22:21:38 -0500

    change vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/taller_portfolio/',
+  base: '/',
   assetsInclude: ["**/*.png", "**/*.PNG"],
   plugins: [react()],
 })
